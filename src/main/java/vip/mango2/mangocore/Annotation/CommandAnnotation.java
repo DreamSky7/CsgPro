ae75e97d4b7e63d450e4434b42e0ead1961244e1
唐七 <10897575+mangorabbit@user.noreply.gitee.com>
2023-09-28 17:01:52 +0800

    初始版本

---
+++ src/main/java/vip/mango2/mangocore/Annotation/CommandAnnotation.java
@@ -0,0 +1,27 @@
+package vip.mango2.mangocore.Annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface CommandAnnotation {
+
+    /* 指令名称 */
+    String name();
+
+    /* 简写指令 */
+    String[] alias() default {};
+
+    /* 指令描述 */
+    String description() default "";
+
+    /* 指令权限 */
+    String permission() default "";
+
+    /* 指令使用方式 */
+    String usage() default "";
+
+}
